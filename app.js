c2655ee0a8659cd7a7ebf1098bffa53353e524af
juanpabloreyesp <hello@juanpabloreyes.cl>
2026-07-09 18:04:21 -0400

    Implement form display function based on type
    Added a function to display forms based on type.

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("MVP iniciado 🚀");
+function mostrarFormulario(tipo){
+
+    document.getElementById("enviar").classList.add("oculto");
+    document.getElementById("viajar").classList.add("oculto");
+
+    if(tipo==="envio"){
+        document.getElementById("enviar").classList.remove("oculto");
+        document.getElementById("enviar").scrollIntoView({behavior:"smooth"});
+    }
+
+    if(tipo==="viaje"){
+        document.getElementById("viajar").classList.remove("oculto");
+        document.getElementById("viajar").scrollIntoView({behavior:"smooth"});
+    }
+
+}
